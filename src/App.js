b1501eb5d363a6f87156f3a2d67c007b77106ec7
Long <longln8@fpt.com.vn>
2021-02-08 17:13:23 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button, ButtonGroup } from '@material-ui/core';
+import  Dashboard  from './Dashboard'
+
+function App() {
+  return (
+    <div>
+        <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
